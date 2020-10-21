2d4e8379dbb5500bb2002dea7b760af8ff84e10e
Kadeem <kletts2000@gmail.com>
2020-10-20 22:12:43 -0400

    Update disaster_planning.py

--- disaster_planning.py
+++ disaster_planning.py
@@ -2,98 +2,135 @@ import urllib.request
 import gzip
 from bs4 import BeautifulSoup
 import mysql.connector
+import pyodbc
 import csv
+import os
 
 # Pulls the data from the address and appends it to the file
-def getFileData(url, filename, outputFile):
-
-
+def getFileData(url, filename):
 	url = url + filename
 	# Open url to file
 	response = urllib.request.urlopen(url)
-	# Append data to output file
-	with open(outputFile, "ab") as outfile:
+	# Append data to output 
+	with open(filename[:-3:], "ab") as outfile:
 		# Skip the first line of the file and write the others to the output file
 		# next(f)
 		outfile.write(gzip.decompress(response.read()))
 
-# Inserts the data from the file into the database
-def insertData(outputFile, conn):
+def loadToDb(conn):
+	pattern = "StormEvents_details-ftp"
+	filenames = [ f for f in os.listdir(".") if pattern in f ]
+	print (filenames)
+
+	# Try to establish a connection to the db
 	try:
-		
+			cursor = conn.cursor()
 
-
-		# Open a cursor
-		cursor = conn.cursor()
-
-		insert_statement = ""
-
-		finalCount = 0
-		headCount = 0
-		print ("Inserting data into the Database.")
-		with open(outputFile, newline="") as csvFile:
-			
-			# Pull contents of file into variable
-			recordReader = csv.reader(csvFile, delimiter=",", quotechar="\"")
-			# recordReader = [w.replace("\"","\\\"") for w in recordReader]
-			header = []
-			# Loop through the data, prepare insert statement and execute
-			for i, row in enumerate(recordReader):
-
-				finalCount = i
-				# Used to get the column names from the file
-				if "EVENT_ID" in row:
-
-					headCount += 1
-					header = row
-					# Find out the number of columns then create enough curly braces {} for formatting
-					column_num = len(header)
-					columns = ("{}," * (column_num-1)) + "{}"
-					# print (columns)
-					
-					# Create first section of insert statment
-					insert_statement = "INSERT INTO ST_STORM_DATA (" + columns + ") VALUES"
-					insert_statement = insert_statement.format(*header)
-					# print (insert_statement)
-
-				if "EVENT_ID" not in row:
-
-					# Escape all double quote " characters that may be in the row
-					row = [w.replace("\"","\\\"") for w in row]
-					# print (row)
-
-					# Find out how many columns of data there are and create enough curly braces for formatting
-					column_num = len(row)
-					columns = ("IFNULL(\"{}\",NULL),"* (column_num-1)) + "IFNULL(\"{}\",NULL)"
-
-					# Create values portion of insert statement
-					values = "(" + columns + ")"
-					values = values.format(*row)
-					values = values.replace("\\\",NULL","\", NULL")
-
-					sql = insert_statement + values
-
-					# Run the query
-					cursor.execute(sql)
-					finalCount =finalCount-headCount
-					if (finalCount % 30000) == 0:
-						conn.commit()
-						print ( "{} Records commited.".format(finalCount))
-
-			print ("Completed")
-
-	
 	except Exception as e:
-		
-		print ("An error occured: {}".format(e))
-		print ("SQL is: " + sql)
-		print (row)
-		conn.rollback()		
+	
+		print("An issue occured {}".format(e))
+		raise
 
-	conn.commit()
-	print ("A total of {} records Inserted".format(finalCount))
+
+	for filename in filenames:
+		file_name = filename
+		try:
+			
+
+
+			# Open a cursor
+
+			insert_statement = ""
+
+			finalCount = 0
+			headCount = 0
+			print ("Inserting {} data into the Database.".format(filename))
+			with open(filename, newline="") as csvFile:
+				
+				# Pull contents of file into variable
+				recordReader = csv.reader(csvFile, delimiter=",", quotechar="\"")
+				# recordReader = [w.replace("\"","\\\"") for w in recordReader]
+				header = []
+				# Loop through the data, prepare insert statement and execute
+				for i, row in enumerate(recordReader):
+
+
+					if i != 0:
+
+						finalCount = i
+						insert_statement = "INSERT INTO ST_STORM_DATA (BEGIN_YEARMONTH,BEGIN_DAY,BEGIN_TIME,END_YEARMONTH,END_DAY,END_TIME,EPISODE_ID,EVENT_ID,STATE,STATE_FIPS,YEAR,MONTH_NAME,EVENT_TYPE,CZ_TYPE,CZ_FIPS,CZ_NAME,WFO,BEGIN_DATE_TIME,CZ_TIMEZONE,END_DATE_TIME,INJURIES_DIRECT,INJURIES_INDIRECT,DEATHS_DIRECT,DEATHS_INDIRECT,DAMAGE_PROPERTY,DAMAGE_CROPS,SOURCE,MAGNITUDE,MAGNITUDE_TYPE,FLOOD_CAUSE,CATEGORY,TOR_F_SCALE,TOR_LENGTH,TOR_WIDTH,TOR_OTHER_WFO,TOR_OTHER_CZ_STATE,TOR_OTHER_CZ_FIPS,TOR_OTHER_CZ_NAME,BEGIN_RANGE,BEGIN_AZIMUTH,BEGIN_LOCATION,END_RANGE,END_AZIMUTH,END_LOCATION,BEGIN_LAT,BEGIN_LON,END_LAT,END_LON,EPISODE_NARRATIVE,EVENT_NARRATIVE,DATA_SOURCE, FILENAME) VALUES"
+						# Escape all double quote " characters that may be in the row
+						row = [w.replace("\"","\\\"") for w in row]
+						# print (row)
+
+						# Find out how many columns of data there are and create enough curly braces for formatting
+						column_num = len(row)
+						columns = ("IFNULL(\"{}\",NULL),"* (column_num-1)) + "IFNULL(\"{}\",NULL)"
+
+						# Create values portion of insert statement
+						values = "(" + columns 
+						values = values.format(*row)
+						values = values +",'"+ file_name  +"')"
+						values = values.replace("\\\",NULL","\", NULL")
+
+						sql = insert_statement + values
+
+						# Run the query
+						cursor.execute(sql)
+						finalCount =finalCount-headCount
+						if (finalCount % 30000) == 0:
+							conn.commit()
+
+				# finalCount -= 1
+				conn.commit()
+				print ("Complete")
+				print ( "{} Records commited.".format(finalCount))
+			csvFile.close()
+
+		
+		except Exception as e:
+			
+			print ("An error occured: {}".format(e))
+			print ("SQL is: " + sql)
+			print (row)
+			moveFile(".","./failed", filename)
+			conn.rollback()
+			raise	
+
+		conn.commit()
+		print ("A total of {} records Inserted".format(finalCount))
+
+		print ("Inserting into log table")
+
+		try:
+
+			sql = "INSERT INTO LOG_STORM_DATA_LOADED (FILENAME, RECORD_COUNT, DATE_LOADED) VALUES ('{}', {}, NOW())".format(file_name, finalCount)
+			cursor.execute(sql)
+			conn.commit()
+			print ("Complete")
+
+		except Exception as e:
+			print ("An error occured: {}".format(e))
+			print ("SQL is: " + sql)
+			conn.rollback()	
+
+		print ("")	
+
+		moveFile(".","./processed", filename)
+
 	cursor.close()
-	conn.close()
+
+
+def moveFile(sourceDir, destDir, filename):
+
+	if os.path.isdir(destDir):
+		os.rename(sourceDir+"/"+filename, destDir + "/" + filename)
+	else:
+		os.makedirs(destDir)
+		os.rename(sourceDir+"/"+filename, destDir + "/" + filename)
+
+
+
 
 # Get the names of the files on the website and store them in a list
 def getFilenames(soup, filenames):
@@ -145,6 +182,19 @@ def answerDamageQuery(summaryFile, conn):
 	except Exception as e:
 		print ("There seems to be an issue: " + e)
 
+def summarizeData(conn):
+
+	try:
+		
+		print ("Summarizing data")
+		cursor = conn.cursor()
+		cursor.callproc("summarize_data")
+		print ("Complete")
+		# conn.close()
+	except Exception as e:
+		print ("There seems to be an issue: {}".format(e))
+
+
 def main():
 
 	url = "https://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/"
@@ -156,6 +206,8 @@ def main():
 		password="p@$$w0rd", 
 		host="localhost", 
 		database="test")
+	# conn = pyodbc.connect('DRIVER={SQL Server};SERVER=LAPTOP-QJCFBNNV;DATABASE=Disaster_Planning;Trusted_Connection=yes')
+
 
 	print ("Getting the file names to be extracted.")
 	# Get the raw html from the page that contains the list of file names
@@ -165,15 +217,41 @@ def main():
 
 	filenames = getFilenames(soup, filenames)
 
+	sql ="SELECT DISTINCT CONCAT(FILENAME, '.gz')FILENAME FROM LOG_STORM_DATA_LOADED"
+
+	try:
+		cursor = conn.cursor()
+		cursor.execute(sql)
+		loadedFilenames = cursor.fetchall()
+		conn.commit()
+
+	except Exception as e:
+		print ("There seems to be an issue: " + str(e))
+
+	# filenames= [f[:-3:] for f in filenames]
+	loadedFilenames = [f[0] for f in loadedFilenames]
+	# print (filenames)
+	# print (loadedFilenames)
+	# print (len(filenames))
+	# print (len(loadedFilenames))
+	filenames = list(set(filenames)- set(loadedFilenames))
+	print (len(filenames))
+
+
+
 	# Extract the data for each file
 	for filename in filenames:
 
 		print ("Retrieving " + filename)
-		getFileData(url, filename, outputFile)
+		getFileData(url, filename)
 		print ("Complete")
 
+
 	# Insert Data into the db
-	insertData(outputFile, conn)
+	loadToDb(conn)
+
+	# Load Fact table
+	summarizeData(conn)
 
 	# Summarize property damage by year and state
 	answerDamageQuery(summaryFile, conn)
